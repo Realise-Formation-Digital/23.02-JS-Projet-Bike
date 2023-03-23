60c3c100509e70e5136ab4231f98e2dbfd69c73e
24Syleo <sylvain.beggiora@outlook.com>
2023-03-23 14:21:54 +0100

    modified:   index.html

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let accessoires = document.getElementsByName('accessories');
+console.log(accessoires);
+let bikePrice = 0;
+let accPrice = 0;
+
+/**
+ * function to get bike
+ * @param {string} - the value of the option
+ */
+
+function getBike(value) {
+    console.log(value);
+    switch (value) {
+        case '1':
+            console.log('1');
+            break;
+        case '2':
+            console.log('2');
+            break;
+        case '3':
+            console.log('3');
+            break;
+        case '4':
+            console.log('4');
+            break;
+        case '5':
+            console.log('5');
+            break;
+    }
+}
